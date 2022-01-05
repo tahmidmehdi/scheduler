4b3bb23b80b7c150136f30868f6e7de6c885964a
tahmidmehdi <tahmid.mehdi@giatec.ca>
2022-01-04 23:30:00 -0500

    added form

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error"
+import Form from "components/Appointment/Form"
 
 
 storiesOf("Button", module)
@@ -163,5 +164,21 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment."
       onClose={action("onClose")}
+    />
+  ))
+  .add("Create", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      name="Lydia Miller-Jones"
+      interviewer={interviewer.id}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ));
